9de91e7ff631fc70353b3f05ea7e1327d850e07e
Sahana2408 <727721euit134@skcet.ac.in>
2023-02-24 16:01:17 +0530

    Day-5

---
+++ BookDetails/src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	private int id;
+	private String name;
+	private float price;
+	private int quantity;
+	private String author;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", price=" + price + ", quantity=" + quantity + ", author="
+				+ author + "]";
+	}
+	
+}
